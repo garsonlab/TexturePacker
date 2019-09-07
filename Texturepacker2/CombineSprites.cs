f2ce2fb88f45e536072d556b33ef0134d476aedf
garsonlab <jiacheng_liu@yeah.net>
2019-09-07 11:29:36 +0800

    fixed

--- Texturepacker2/CombineSprites.cs
+++ Texturepacker2/CombineSprites.cs
@@ -3,10 +3,11 @@ using System.IO;
 using UnityEditor;
 using UnityEngine;
 
+/// <summary>
+/// 一键打图集并切割
+/// </summary>
 public class CombineSprites : Editor
 {
-    // 是否去除原始图片周围的透明
-    private static bool _trimBorder = true;
 
     [MenuItem("Tools/CustomAtlas")]
     static void Combine()
@@ -18,12 +19,8 @@ public class CombineSprites : Editor
             return;
         }
 
+        // 是否去除原始图片周围的透明
         if (EditorUtility.DisplayDialog("提示", "是否去除原图片透明边框, 去除后可能会减小图集大小", "当然", "算了"))
-            _trimBorder = true;
-        else
-            _trimBorder = false;
-
-        if (_trimBorder)
         {
             for (int i = 0; i < sprites.Length; i++)
             {
@@ -31,50 +28,50 @@ public class CombineSprites : Editor
             }
         }
 
+        string path = EditorUtility.SaveFilePanel("SavePath", "Assets/Res", "atlas", "png");
+        if (string.IsNullOrEmpty(path))
+            return;
+
         Texture2D texture = new Texture2D(1024, 1024, TextureFormat.ARGB32, false);
         Rect[] rects = texture.PackTextures(sprites, 1);
 
-        string path = EditorUtility.SaveFilePanel("SavePath", "Assets/Res", "atlas", "png");
-        if (!string.IsNullOrEmpty(path))
-        {
-            texture.Apply();
-            File.WriteAllBytes(path, texture.EncodeToPNG());
-            if (!path.Contains("Assets/"))
-                return;
+        texture.Apply();
+        File.WriteAllBytes(path, texture.EncodeToPNG());
+        if (!path.Contains("Assets/"))
+            return;
 
-            path = "Assets/" + path.Split(new []{"Assets/"}, StringSplitOptions.None)[1];
-            AssetDatabase.ImportAsset(path);
-            AssetDatabase.Refresh();
+        path = "Assets/" + path.Split(new []{"Assets/"}, StringSplitOptions.None)[1];
+        AssetDatabase.ImportAsset(path);
+        AssetDatabase.Refresh();
             
-            var importer = (TextureImporter)AssetImporter.GetAtPath(path);
-            importer.textureType = TextureImporterType.Sprite;
-            importer.spriteImportMode = SpriteImportMode.Multiple;
+        var importer = (TextureImporter)AssetImporter.GetAtPath(path);
+        importer.textureType = TextureImporterType.Sprite;
+        importer.spriteImportMode = SpriteImportMode.Multiple;
 
-            int width = texture.width;
-            int height = texture.height;
+        int width = texture.width;
+        int height = texture.height;
 
-            SpriteMetaData[] datas = new SpriteMetaData[rects.Length];
-            for (int j = 0; j < rects.Length; j++)
-            {
-                Rect rect = rects[j];
-                rect.x *= width;
-                rect.y *= height;
-                rect.width *= width;
-                rect.height *= height;
+        SpriteMetaData[] datas = new SpriteMetaData[rects.Length];
+        for (int j = 0; j < rects.Length; j++)
+        {
+            Rect rect = rects[j];
+            rect.x *= width;
+            rect.y *= height;
+            rect.width *= width;
+            rect.height *= height;
 
-                var data = new SpriteMetaData();
-                data.name = sprites[j].name;
-                data.rect = rect;
-                data.pivot = Vector2.one*0.5f;
-                data.border = Vector4.zero;
-                datas[j] = data;
-            }
-
-            importer.spritesheet = datas;
-            AssetDatabase.ImportAsset(path);
-
-            Selection.activeObject = AssetDatabase.LoadMainAssetAtPath(path);
+            var data = new SpriteMetaData();
+            data.name = sprites[j].name;
+            data.rect = rect;
+            data.pivot = Vector2.one*0.5f;
+            data.border = Vector4.zero;
+            datas[j] = data;
         }
+
+        importer.spritesheet = datas;
+        AssetDatabase.ImportAsset(path);
+
+        Selection.activeObject = AssetDatabase.LoadMainAssetAtPath(path);
     }
 
     static Texture2D TrimBorder(Texture2D texture)
